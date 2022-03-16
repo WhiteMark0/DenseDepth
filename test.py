9475602119c7cc7b249b9da8742c247474c5680d
WhiteMark0 <58772124+WhiteMark0@users.noreply.github.com>
2022-03-16 20:40:44 +0800

    Update test.py

--- test.py
+++ test.py
@@ -28,7 +28,7 @@ model = load_model(args.model, custom_objects=custom_objects, compile=False)
 print('\nModel loaded ({0}).'.format(args.model))
 
 # Input images
-inputs = load_images( glob.glob(args.input) )
+inputs = load_images( glob.glob('rabbit') )
 print('\nLoaded ({0}) images of size {1}.'.format(inputs.shape[0], inputs.shape[1:]))
 
 # Compute results
